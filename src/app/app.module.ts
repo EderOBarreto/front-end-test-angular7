146f8b2a0950be00214117adea92ca325cc57c3f
Eder <ederoliveirabarreto@gmail.com>
2019-03-19 15:48:20 -0300

    Layout Register Employee Page Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeRegisterComponent } from './employee-register/employee-register.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeItemComponent } from './employee-list/employee-item/employee-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeRegisterComponent,
+    EmployeeListComponent,
+    EmployeeItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employee-list/employee-item/employee.model.ts
@@ -0,0 +1,13 @@
+export class Employee {
+    public name: string;
+    public age: number;
+    public dataNasc: Date;
+    public phone: string;
+    public rg: string;
+    public cpf: string;
+    public address: string;
+    public addressNumber: number;
+    public city: string;
+    public state: string;
+    public notes: string;
+}
